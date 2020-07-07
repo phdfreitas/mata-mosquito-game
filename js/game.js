c0386082d257b1758227deba0553ac96b5be42bc
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-07-07 11:39:45 -0300

    Tamanho e Lado

--- js/game.js
+++ js/game.js
@@ -13,17 +13,41 @@ tamanhoTela()
 // Criação de elementos randomicos
 function posicaoRandomica(){
 	
-	var posY = Math.abs(Math.floor(Math.random() * altura) - 90)
-	var posX = Math.abs(Math.floor(Math.random() * largura) - 90)
+	var posY = Math.abs(Math.floor(Math.random() * altura) - 120)
+	var posX = Math.abs(Math.floor(Math.random() * largura) - 120)
 
 	console.log(posX, posY)
 
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = 'mosquito'
+	mosquito.className = tamanhoAleatorio() + ' ' + inverteLado() // Associa o retorno das funções as classes css
 	mosquito.style.left = posX+'px'
 	mosquito.style.top = posY+'px'
 	mosquito.style.position = 'absolute'
 	document.body.appendChild(mosquito)
 }
 posicaoRandomica()
+
+// Altera o tamanho dos mosquitos exibidos na tela
+function alteraTamanho(){
+	var classe = Math.floor(Math.random() * 3)
+	switch (classe) {
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
+	}
+}
+
+// Altera o lado para o qual o mosquito "olha"
+function alteraLado(){
+	var classe = Math.floor(Math.random() * 2)
+	switch (classe) {
+		case 0:
+			return 'ladoEsquerdo'
+		case 1:
+			return 'ladoDireito'
+	}
+}
